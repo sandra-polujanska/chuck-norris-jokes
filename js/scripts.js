5613331990ff5cd4a36059b0b77ec198e33ebcca
Sandra Połujańska <sandra.polujanska@gmail.com>
2018-03-24 22:17:35 +0100

    Changes

--- js/scripts.js
+++ js/scripts.js
@@ -2,21 +2,21 @@
 
 var url = 'http://api.icndb.com/jokes/random';
 
-var button = document.getElementById('get-joke');
-button.addEventListener('click', function(){
-  getJoke();
+var $button = $('#get-joke').click(function() {
+	getJoke();
 });
 
-var paragraph = document.getElementById('joke');
+var $paragraph = $('#joke');
 
 function getJoke() {
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', url);
-  xhr.addEventListener('load', function(){
-    var response = JSON.parse(xhr.response);
-    paragraph.innerHTML = response.value.joke;
-  });
-  xhr.send();
+$.ajax({
+	method: 'GET',
+	url: url, //here's a weird construction, but on the left we have the name of the parameter, and to the right is the name of the variable that holds the value
+	success: function(res) {
+		$paragraph.text(res.value.joke);
+	}
+
+});
 }
 
 document.addEventListener("DOMContentLoaded", getJoke());
