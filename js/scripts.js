c6c0de863a8b85c6d8636a1a42923c2dfce01728
Sandra Połujańska <sandra.polujanska@gmail.com>
2018-03-24 22:34:42 +0100

    Changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 // scripts.js
 
-var url = 'http://api.icndb.com/jokes/random';
+var url = 'https://api.icndb.com/jokes/random';
 
 var $button = $('#get-joke').click(function() {
 	getJoke();
@@ -11,7 +11,7 @@ var $paragraph = $('#joke');
 function getJoke() {
 $.ajax({
 	method: 'GET',
-	url: url, //here's a weird construction, but on the left we have the name of the parameter, and to the right is the name of the variable that holds the value
+	url: url,
 	success: function(res) {
 		$paragraph.text(res.value.joke);
 	}
